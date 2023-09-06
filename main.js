f968352eec8fc24a115a7e52c8061a30c58a3730
ChrisGlz14 <christiang14@hotmail.com.ar>
2023-09-06 19:03:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,129 @@
+// for (let i = 1; i <= 2; i++) {
+
+//     numeros = Number(prompt("Ingrese un numero aleatorio"))
+
+//     console.log(i)
+//     console.log(numeros)
+// }
+
+// let pesos = 36500
+
+// let dolar = 730
+
+// let conversion = pesos / dolar
+
+// console.log("El total de PESOS :"+ pesos + " Es igual a :" + conversion + " DOLARES")
+
+// acumulador = 0
+
+// for (let i = 0; i < 3; i++) {
+
+//     // ACA VAN LAS OPERACIONES
+//     let nota1 = Number(prompt("Ingrese el valor de las notas"));
+//     acumulador = acumulador + nota1
+
+// }
+
+// const promedio = acumulador / 3;
+
+// console.log("La suma de las nota son: " + acumulador);
+// console.log("El promedio de las notas son: " + promedio);
+
+// const edades = [5, 8, 9, 6, 7, 15, 99, 48, 84, 12];
+
+// let acumulador = 0
+
+// for (let i = 0; i < edades.length; i++) {
+//   const element = edades[i];
+
+//   acumulador = acumulador + edades [i]
+
+// }
+
+// let promedio = acumulador / edades.length
+
+// console.log("El promedio de edad es de: " + promedio)
+
+  // let numeros = []
+  // let maximo = 0
+
+  // const cantidadDeNumeros = 10
+
+  // for (let i = 0; i < cantidadDeNumeros; i++) {
+    
+  //   let numero = Number(prompt("Ingrese un numero"));
+  //   numeros.push(numero);
+
+
+  //   if (numero > maximo) {
+  //     maximo = numero
+  //   }
+  // }
+  // console.log("El numero se encuentra en la posicion del array numero: "+ numeros.indexOf(maximo))
+    
+  // console.log("Los números ingresados son:", numeros);
+  // console.log("El número máximo es:", maximo)
+
+
+// let numeros = [];
+// let positivos = 0
+// const cantidadDeNumeros = 6;
+
+// for (let i = 0; i < cantidadDeNumeros; i++) {
+
+//   let numero = Number(prompt("Ingrese un numero"));
+//   numeros.push(numero)
+//   if (numero > 0) {
+//   positivos++
+//   }
+// }
+
+// console.log("La cantidad de numeros positivos son: "+ positivos)
+
+// let numeros = [];
+// let minimo = Infinity;
+
+// const cantidadDeNumeros = 4;
+
+// for (let i = 0; i < cantidadDeNumeros; i++) {
+
+// let numero = Number(prompt("Ingrese un numero"));
+// numeros.push(numero);
+
+// if (numero < minimo) {
+
+//   minimo = numero;
+
+//   }
+// }
+// console.log("El numero minimo es: "+ minimo)
+
+
+// let edades = []
+
+// let sumaMayorEdad = 0
+
+// for (let i = 0; i < 4; i++) {
+  
+//   let edad = Number(prompt("Ingrese un numero"));
+
+//   edades.push(edad);
+//   acumulador = edad
+  
+//   if (edad > 18) {
+//     sumaMayorEdad += edad
+//   }
+// }
+// let cantidadMayores = edades.filter(edad => edad > 18).length;
+
+// if(cantidadMayores > 0) {
+//   let promedio = sumaMayorEdad / cantidadMayores
+//   console.log("Edades ingresadas:", edades);
+//   console.log("Promedio de edades mayores de 18 años:", promedio);
+// } else {
+//   console.log("Ingrese datos porfavor")
+// }
+
+
+
+
